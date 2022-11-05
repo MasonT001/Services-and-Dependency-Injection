ce806fff2ceb69e023b8f8763e2a207ea8c434e2
MasonT001 <mltrepagnier@gmail.com>
2022-11-05 10:11:50 -0700

    fixed bug

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -21,11 +21,13 @@ export class AccountsService {
       constructor(private loggingService: LoggingService) {}
 
       addAccount(name: string, status: string) { 
-        this.accounts.push({name: name, status: status});  
+        this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChanged(status)  
     }
 
       updateStatus(id: number, status: string) {
         this.accounts[id].status = status
+        this.loggingService.logStatusChanged(status)
       }
 
     }
